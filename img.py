b8d23aa562da661b72633fdad8e12f54bba38743
seagap <953556780@qq.com>
2019-08-02 16:45:38 +0800

    testv.3

--- img.py
+++ img.py
@@ -6,62 +6,66 @@ from keras import optimizers
 
 train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
+
 train_dir='D:\cv\\trainset'
 train_generator = train_datagen.flow_from_directory(
-train_dir,
-target_size=(20,20),
-batch_size=3000,
-class_mode='binary')
+    train_dir,
+    target_size=(20,20),
+    batch_size=1000,
+    class_mode='binary')
+
 validation_dir='D:\cv\\validation'
 validation_generator = test_datagen.flow_from_directory(
-validation_dir,
-target_size=(20,20),
-batch_size=500,
-class_mode='binary')
+    validation_dir,
+    target_size=(20,20),
+    batch_size=100,
+    class_mode='binary')
+
 for data_batch, labels_batch in train_generator:
     print('data batch shape:', data_batch.shape)
     print('labels batch shape:', labels_batch.shape)
     break
+
+for data_batch, labels_batch in validation_generator:
+    print('vadata batch shape:', data_batch.shape)
+    print('valabels batch shape:', labels_batch.shape)
+    break
+
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',
-input_shape=(20, 20, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+    input_shape=(20, 20, 3)))
 model.add(layers.Flatten())
-model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
+print(model.summary())
 
 model.compile(loss='binary_crossentropy',
-optimizer=optimizers.RMSprop(lr=1e-4),
-metrics=['acc'])
+    optimizer=optimizers.RMSprop(lr=1e-4),
+    metrics=['acc'])
 
 history = model.fit_generator(
-train_generator,
-steps_per_epoch=100,
-epochs=30,
-validation_data=validation_generator,
-validation_steps=50)
+    generator=train_generator,
+    verbose=1,
+    steps_per_epoch=3000,
+    epochs=5,
+    validation_data=validation_generator,
+    validation_steps=500)
+print(history)
 model.save('cats_and_dogs_small_1.h5')
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(acc) + 1)
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(1, len(acc) + 1)
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.legend()
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
 #test
 #ok
