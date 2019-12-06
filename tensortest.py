e54c27ae07a38fb89a64a9f20ef26b7201e740b0
seagap <953556780@qq.com>
2019-12-06 16:27:13 +0800

    V12-6

--- tensortest.py
+++ tensortest.py
@@ -65,26 +65,29 @@ def mutilayers(x):
                         num_outputs=1, trainable=False, )
     ly5 = ly1 + ly2 + ly3 + ly4
     ly6 = layers.max_pool2d(ly5, kernel_size=([3, 3]))
-    ly7 = layers.fully_connected(ly6,activation_fn=tf.nn.relu,num_outputs=1)
-    # ly8=tf.reshape(ly7,[9,-1,16])
-    # right layer
-    ly8 = tf.reshape(ly6, [9, -1, 1])
-    ly9 = layers.fully_connected(ly8,activation_fn=tf.nn.relu, num_outputs=1)
-    test=tf.reshape(ly9,[9,-1])
-    ly10 = layers.fully_connected(test,activation_fn=tf.nn.relu, num_outputs=3)
-    # leftout=layers.dense_to_sparse()
-    #kernel
-    # ly11 = layers.fully_connected(ly6,activation_fn=tf.nn.relu, num_outputs=256)
-    # ly12 = layers.fully_connected(ly11, activation_fn=tf.nn.relu, num_outputs=256)
-    # ly13 = tf.reshape(ly12, [9, 16, -1])
-    # outmatrix1=tf.matmul(ly8,ly13)
-    # outmatrix2 = tf.matmul(outmatrix1, ly10)
-    # outputs=layers.conv2d(ly6,kernel_size=tuple([49,83]), stride=([1,1]), padding='SAME',
-    #                     weights_initializer=tf.initializers.constant(outmatrix2),num_outputs=1)
-    # ly10=ly9[0][0][0][0]   #Dose it have batch var ?  should not have
-    return ly10
+    ly7 = tf.reshape(ly6, [-1, 4067])
+    return ly7
 
 
+def leftM(x):
+    ly9 = layers.fully_connected(x, activation_fn=tf.nn.relu, num_outputs=4067)
+    ly10 = layers.fully_connected(ly9, activation_fn=tf.nn.relu, num_outputs=784)
+    leftout = tf.reshape(ly10, [-1, 49, 16])
+    return leftout
+
+
+def kernelM(x):
+    ly11 = layers.fully_connected(x, activation_fn=tf.nn.relu, num_outputs=256)
+    ly12 = layers.fully_connected(ly11, activation_fn=tf.nn.relu, num_outputs=256)
+    kernel = tf.reshape(ly12, [-1, 16, 16])
+    return kernel
+
+
+def rightM(x):
+    ly15 = layers.fully_connected(x, activation_fn=tf.nn.relu, num_outputs=4067)
+    ly16 = layers.fully_connected(ly15, activation_fn=tf.nn.relu, num_outputs=1328)
+    rightout = tf.reshape(ly16, [-1, 16, 83])
+    return rightout
 #
 # #-------------------------------------------------------
 # # x = tf.placeholder(dtype=tf.float64, shape=[None, 300, 500, 3], name='x')
@@ -101,21 +104,24 @@ with graph.as_default():
     y = tf.placeholder(dtype=tf.float64, shape=[None], name='y')
     # img.dtype='float'
     # print(img)
-
-    y_hat = mutilayers(x / 255.0)
+    featuremap = mutilayers(x / 255.0)
+    matrixs =tf.matmul(tf.matmul(leftM(featuremap),kernelM(featuremap)),rightM(featuremap))
+    y_hat=matrixs*x
     loss = y_hat - y
-    # train = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss)
+    train = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss)
     init = tf.global_variables_initializer()
+    '''
     with tf.Session() as sess:
         sess.run(init)
         for i in range(1, 2):
             x_batch, y_batch = generdata()
             # print(np.shape(x_batch))
             # print(np.shape(y_batch))
-            # sd = sess.run([loss, train], feed_dict={x: x_batch, y: y_batch})
-            print(np.shape(sess.run(y_hat, feed_dict={x: x_batch})))
+            sd = sess.run([loss, train], feed_dict={x: x_batch, y: y_batch})
+            # print(np.shape(sess.run(y_hat, feed_dict={x: x_batch})))
+    '''
 # correct_prediction = tf.equal(y_hat, y)
 # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 # xtest_batch, ytest_batch = generdata()
 # print("Accuracy：", accuracy.eval({x: xtest_batch, y: ytest_batch}))
-# file_writer = tf.summary.FileWriter('./log3', graph)
+    file_writer = tf.summary.FileWriter('./log4', graph)
